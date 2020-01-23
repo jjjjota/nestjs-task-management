7b57d61a62bc8d4214170910b643a3fcf70346ee
Javiera Jopia <javiera.jopia@magnet.cl>
2020-01-23 18:43:21 -0300

    feat: add get all taks get controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // If this property is not private, any other component from the outside
+  // that injects this service will be able to modify it
+  private tasks = [];
+
+  getAllTasks () {
+    return this.tasks;
+  }
+}
