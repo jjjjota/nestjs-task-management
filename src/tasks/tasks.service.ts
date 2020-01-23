a4668c09d5e52537782f4cfc64bf0d27d5c27470
Javiera Jopia <javiera.jopia@magnet.cl>
2020-01-23 19:10:31 -0300

    feat: define task model/interface

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './tasks.model';
 
 @Injectable()
 export class TasksService {
   // If this property is not private, any other component from the outside
   // that injects this service will be able to modify it
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  getAllTasks () {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
