808f1db6d5eaa221b580ac7c0f4addc4d9a7bf9a
Javiera Jopia <javiera.jopia@magnet.cl>
2020-01-24 12:44:49 -0300

    feat: create a custom validaiton pipe for update status

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "../tasks.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  // A pipe allways have to have a transform method
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${ value }" is an invalid status.`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    return this.allowedStatuses.indexOf(status) !== -1;
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -46,10 +47,12 @@ export class TasksController {
     this.tasksService.deleteTask(id);
   }
 
+  // NOTE: NestJS will create a new instance of TaskStatusValidationPipe,
+  // so is not necessary to do it
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
