eedb26808d96cfcdd436131541a9223444d38ac0
Javiera Jopia <javiera.jopia@magnet.cl>
2020-01-27 13:46:13 -0300

    feat: hash password for user sigup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,8 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,8 +11,21 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') { // duplicated username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
